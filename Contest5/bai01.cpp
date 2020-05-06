feaca100a7be64fbd1298c0a079bedfdcf6993b2
= <=>
2020-05-06 22:46:48 +0700

    CTDL&GT Contest5

---
+++ Contest5/bai01.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define i64 long long
+#define fi first
+#define se second
+#define rz resize
+#define pb push_back
+#define mp make_pair
+#define whatIs(a) cout << #a " is " << (a) << endl;
+ 
+const int N = 200005;
+const long long base = 1e9+7;
+const long long inf = 1e18+7;
+ 
+using namespace std;
+ 
+int Dp[2005][2005];
+ 
+void Solve(){
+    string a,b;
+    memset(Dp,0,sizeof(Dp));
+    cin >> a;
+    cin.ignore();
+    cin >> b;
+    int n=a.size();
+    int m=b.size();
+    a=' '+a;
+    b=' '+b;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(a[i]==b[j]) Dp[i][j]=Dp[i-1][j-1]+1;
+            else Dp[i][j]=max(Dp[i-1][j],Dp[i][j-1]);
+        }
+    }
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=m;j++){
+    //         cout << Dp[i][j]<<" ";
+    //     }
+    //     cout << endl;
+    // }
+    cout << Dp[n][m]<<endl;
+}
+int main(){
+    int T=1;
+    cin >> T;
+    while(T--){
+        Solve();
+    }
+    return 0;
+} 
---
+++ Contest5/bai03.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define i64 long long
+#define fi first
+#define se second
+#define rz resize
+#define pb push_back
+#define mp make_pair
+#define whatIs(a) cerr << #a " is " << (a) << endl;
+ 
+const int N = 200005;
+const long long base = 1e9+7;
+const long long inf = 1e18+7;
+ 
+using namespace std;
+int L[N]={0};
+void Solve(){
+    for(int i=0;i<N;i++) L[i]=0;
+    int n,s;
+    cin >> n >> s;
+    int a[n+5];
+    L[0]=1;
+    for(int i=1;i<=n;i++) cin >> a[i];
+    for(int i=1;i<=n;i++){
+        for(int t=s;t>=a[i];t--){
+            if(L[t]==0 && L[t-a[i]]==1) L[t]=1;
+        }
+    }
+    if(L[s]==1) cout << "YES";
+    else cout << "NO";
+    cout << endl;
+}
+ 
+int main(){
+    int T=1;
+    cin >> T;
+    while(T--){
+        Solve();
+    }
+    return 0;
+}
